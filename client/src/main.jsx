e30ceca8cb614b01ad6eb50f90543582263c64e2
Jorge <jorestuard@gmail.com>
2023-10-16 18:24:02 +0200

    feat: merge feature/create-card into feature/router-dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import './styles/css/reset.css'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/Home.jsx'
-import './styles/css/reset.css'
+
 import ProductsCards from './pages/ProductsCards'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Home />
     <ProductsCards />
   </React.StrictMode>,
 )
