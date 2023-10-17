918c6a7ee7e430946b1f3158ed7307634b4d932c
Victor-N <victornashillustration@gmail.com>
2023-10-17 20:24:07 +0200

    fix(backend)last-try-starting-again

--- server/app.ts
+++ server/app.ts
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 
-//👀 importante este paso para poder parar el servidor dentro de los test
+
 const server = app.listen(8000,() =>{
 console.log('🚀server up in http://localhost:8000/')
 } )
