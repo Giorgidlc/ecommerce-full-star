5e7fd45a2772c7bf5fd61a2743fce7fffee730e3
thuchagas <thuachagascorreia@gmail.com>
2023-10-24 10:26:52 +0100

    feat: create navbardashboard

---
+++ client/src/components/NavbarDashboard.jsx
@@ -0,0 +1,19 @@
+
+
+export const NavbarDashboard = () => {
+  return (
+    <nav className="admin-navbar">
+      <section className="admin-navbar-button">
+        <button
+          type="button" 
+          id="home-button">
+          <img src="../src/assets/Home-Button.svg" alt="Home" />
+        </button>
+      </section>
+      <h2 className="titleDashboard">Cookies & Dreams Dashboard</h2>
+      <section className="admin-navbar-logo">
+        <img src="../src/assets/Logo.svg" alt="Logo" />
+      </section>
+    </nav>
+  );
+};
---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import '../styles/css/dashboard.css'
+import { NavbarDashboard } from "../components/NavbarDashboard";
+
+const Dashboard = () => {
+    return(
+        <section>
+            <NavbarDashboard />
+        </section>
+    )
+}
+
+export default Dashboard
